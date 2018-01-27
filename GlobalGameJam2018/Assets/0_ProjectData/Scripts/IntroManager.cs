cc4192f4e8d0104ac4eeef9463d5cdc79b008671
CabbageWarrior <feelthecabbage@gmail.com>
2018-01-27 12:30:20 +0100

    Aggiunta scena Intro+Credits e scene nella lista del Build.

---
+++ GlobalGameJam2018/Assets/0_ProjectData/Scripts/IntroManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroManager : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void GoToGame()
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+        Debug.Log("Sei uscito!");
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
