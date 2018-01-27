812fff5d8958671db879bd787fe5366c1dfdb5c4
CabbageWarrior <feelthecabbage@gmail.com>
2018-01-27 18:38:21 +0100

    Aggiunto script di profondità e ordinamento sprite. Aggiunto prefab Fiamma. Aggiunta struttura cartelle degli audio.

---
+++ GlobalGameJam2018/Assets/0_ProjectData/Scripts/DepthSorting.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class DepthSorting : MonoBehaviour
+{
+    Renderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        rend.sortingOrder = GlobalMethods.GetSortingOrderFromPositionY(transform.position.y);
+    }
+}
---
+++ GlobalGameJam2018/Assets/0_ProjectData/Scripts/GlobalMethods.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GlobalMethods
+{
+    public static int GetSortingOrderFromPositionY(float positionY) {
+        return (int)((positionY * -1 * 100) + 1000);
+    }
+}
