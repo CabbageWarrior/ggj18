255159089395cbc8509c9bf8ef2bd0989043f534
CabbageWarrior <feelthecabbage@gmail.com>
2018-01-27 20:30:57 +0100

    Aggiunti primi test audio e mappatura dei tasti per i coristi.

---
+++ GlobalGameJam2018/Assets/AudioCoro.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioCoro : MonoBehaviour
+{
+    public AudioSource audioGiusto;
+    public AudioSource audioSbagliato;
+
+    public AudioClip[] audioClips;
+
+    public KeyCode[] triggerButtons;
+
+    [Space]
+    public bool canPressNow = false;
+
+    private bool correctButtonPressed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (KeyCode keyCode in triggerButtons)
+        {
+            if (Input.GetKeyDown(keyCode))
+            {
+                if (canPressNow)
+                {
+                    StartCorrectAudio();
+                }
+                else
+                {
+                    StartSfigatedAudio();
+                }
+            }
+
+            if (Input.GetKeyUp(keyCode))
+            {
+                StopCorrectAudio();
+            }
+        }
+
+
+    }
+
+    private void StartCorrectAudio()
+    {
+        if (!audioGiusto.isPlaying)
+        {
+            if (audioSbagliato.isPlaying)
+            {
+                audioSbagliato.Stop();
+            }
+
+            audioGiusto.Play();
+        }
+    }
+
+    private void StopCorrectAudio()
+    {
+        if (audioGiusto.isPlaying)
+        {
+            audioGiusto.Stop();
+        }
+    }
+
+    private void StartSfigatedAudio()
+    {
+        if (true)// (!audioSbagliato.isPlaying)
+        {
+            StopCorrectAudio();
+
+            audioSbagliato.clip = audioClips[UnityEngine.Random.Range(0, audioClips.Length)];
+            audioSbagliato.Play();
+        }
+    }
+}
